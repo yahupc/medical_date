36a08754c0001c877dd3b1ed39fe37ff35a5c215
yahu39 <yahu39pc@gmail.com>
2022-11-17 01:34:35 -0500

    Jetstream authentication

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,50 @@
+@extends('adminlte::page')
+
+@section('title', 'General Web')
+
+@section('content_header')
+<h1>General Web</h1>
+@stop
+
+@section('content')
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+<p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+<link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+<script>
+  console.log('Hi!');
+</script>
+@stop
